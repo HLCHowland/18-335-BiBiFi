3542130ba54f5c2eabdc92cd4998e712b48e8af1
HLCHowland <hlchowland@gmail.com>
2022-02-13 15:17:39 -0500

    In place file encryption written, requires testing

--- crypto.c
+++ crypto.c
@@ -1,27 +1,117 @@
 #include <stdio.h>
 #include <sodium.h>
+#include <string.h>
+#include <stdlib.h>
+#include <math.h>
 // #include "fileEncryptionTest.h"
 
 #define CHUNK_SIZE 4096
+#define CRYPTO_HEADER_SIZE 24
+#define CRYPTO_ABYTE_SIZE 17
+#define CRYPTO_FINAL_SIZE 3
+
+// printf("HEADER_BYTES: %i\n", crypto_secretstream_xchacha20poly1305_HEADERBYTES);
+// printf("ABYTES: %i\n", crypto_secretstream_xchacha20poly1305_ABYTES);
+// printf("TAG_FINAL: %i\n", crypto_secretstream_xchacha20poly1305_TAG_FINAL);
 
 
-char* file_to_buffer(char* source_file){
+
+// char* dncrypt_buffer(char *source_buffer, int file_size, const unsigned char key[crypto_secretstream_xchacha20poly1305_KEYBYTES]){
+
+//     char * encrypted_buffer;
+//     double total_chunks = 0;
+//     unsigned long long out_len;
+//     int buffer_offset = 0;
+//     total_chunks = (double)file_size/(double)CHUNK_SIZE;
+
+
+//     // This is done because if total_chunks is a fraction, that means there was an additional chunk
+//     if(total_chunks != floor(total_chunks)){
+//         total_chunks = floor(total_chunks) + 1;
+//     }
+    
+//     // printf("Total Chunks: %f\n", total_chunks);
+//     int encrypted_buffer_size = 0;
+//     encrypted_buffer_size = (CRYPTO_HEADER_SIZE + CRYPTO_FINAL_SIZE + (total_chunks * CRYPTO_ABYTE_SIZE) + file_size);
+        
+//     encrypted_buffer = malloc(encrypted_buffer_size);
+
+//     unsigned char  buf_in[CHUNK_SIZE];
+//     // unsigned char  encrypted_buffer[CHUNK_SIZE + crypto_secretstream_xchacha20poly1305_ABYTES];
+//     unsigned char  header[crypto_secretstream_xchacha20poly1305_HEADERBYTES];
+//     crypto_secretstream_xchacha20poly1305_state st;
+//     crypto_secretstream_xchacha20poly1305_init_push(&st, header, key);
+//     printf("header: %s\n", header);
+
+//     memcpy(&encrypted_buffer[buffer_offset], &header, CRYPTO_HEADER_SIZE + 1);
+//     printf("Encrypted Data Stage 1: %s\n", encrypted_buffer);
+//     buffer_offset = buffer_offset + CRYPTO_HEADER_SIZE;
+
+//     // Writes encrypted body to the buffer
+//     crypto_secretstream_xchacha20poly1305_push(&st, &encrypted_buffer[buffer_offset], &out_len, source_buffer, file_size, NULL, 0, crypto_secretstream_xchacha20poly1305_TAG_FINAL);
+//     printf("Encrypted Data Stage 2: %s\n", encrypted_buffer);
+
+// }
+
+
+
+
+
+char* encrypt_buffer(char *source_buffer, int file_size, const unsigned char key[crypto_secretstream_xchacha20poly1305_KEYBYTES]){
+
+    char * encrypted_buffer;
+    double total_chunks = 0;
+    unsigned long long out_len;
+    int buffer_offset = 0;
+    total_chunks = (double)file_size/(double)CHUNK_SIZE;
+
+
+    // This is done because if total_chunks is a fraction, that means there was an additional chunk
+    if(total_chunks != floor(total_chunks)){
+        total_chunks = floor(total_chunks) + 1;
+    }
+    
+    // printf("Total Chunks: %f\n", total_chunks);
+    int encrypted_buffer_size = 0;
+    encrypted_buffer_size = (CRYPTO_HEADER_SIZE + CRYPTO_FINAL_SIZE + (total_chunks * CRYPTO_ABYTE_SIZE) + file_size);
+        
+    encrypted_buffer = malloc(encrypted_buffer_size);
+
     unsigned char  buf_in[CHUNK_SIZE];
-    FILE          *fp_t, *fp_s;
+    // unsigned char  encrypted_buffer[CHUNK_SIZE + crypto_secretstream_xchacha20poly1305_ABYTES];
+    unsigned char  header[crypto_secretstream_xchacha20poly1305_HEADERBYTES];
+    crypto_secretstream_xchacha20poly1305_state st;
+    crypto_secretstream_xchacha20poly1305_init_push(&st, header, key);
+    printf("header: %s\n", header);
+
+// Yes, the header in the file and the buffer are identical, so this will work
+    FILE *fp_t;
+    fp_t = fopen("testout", "wb");
+    fwrite(header, 1, sizeof header, fp_t);
+
+    memcpy(&encrypted_buffer[buffer_offset], &header, CRYPTO_HEADER_SIZE + 1);
+    printf("Encrypted Data Stage 1: %s\n", encrypted_buffer);
+    buffer_offset = buffer_offset + CRYPTO_HEADER_SIZE;
+
+    // Writes encrypted body to the buffer
+    crypto_secretstream_xchacha20poly1305_push(&st, &encrypted_buffer[buffer_offset], &out_len, source_buffer, file_size, NULL, 0, crypto_secretstream_xchacha20poly1305_TAG_FINAL);
+    printf("Encrypted Data Stage 2: %s\n", encrypted_buffer);
+
+    return encrypted_buffer;
+}
+
+
+//   NEED TO DO A MEMSET 0 HERE
+char* file_to_buffer(char* source_file, int file_size){
+    unsigned char  buf_in[CHUNK_SIZE];
     size_t         read_len;
     int            eof;
-    int file_size;
+    FILE *fp_s;
     int file_offset = 0;
-
     char *mem_file;
 
-    printf("Sourcefile: %s\n", source_file);
-
-    fp_s = fopen("script", "rb");
-    fseek(fp_s, 0L, SEEK_END);
-    file_size = ftell(fp_s);
-    printf("file size: %i", file_size);
-    fclose(fp_s);
+    // printf("Sourcefile: %s\n", source_file);
+    // printf("file size: %i", file_size);
     mem_file = malloc(file_size);
     
     // Check for null pointers
@@ -47,9 +137,40 @@ char* file_to_buffer(char* source_file){
 
 
 int main(){
+
     char * mem_file = NULL;
-    char source_file[] = "script";
-    mem_file = file_to_buffer(source_file);
+    char * encrypted_buffer = NULL;
+    char * decrypted_buffer = NULL;
+    
+    char source_file[] = "testFileLarge";
+
+    // unsigned char key[crypto_secretstream_xchacha20poly1305_KEYBYTES];
+    unsigned char key[] = "mmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmm";
+
+    // We need to find the file size on the outside of the function because we will
+    // have no idea how long the buffered file is without it since you can't
+    // derive that by scanning the malloced area of memory
+
+    FILE *fp_s;
+    int file_size = 0;
+    fp_s = fopen(source_file, "rb");
+    fseek(fp_s, 0L, SEEK_END);
+    file_size = ftell(fp_s);
+
+    mem_file = file_to_buffer(source_file, file_size);
+
+
+    // crypto_secretstream_xchacha20poly1305_keygen(key);
+    printf("Key Size %i",crypto_secretstream_xchacha20poly1305_KEYBYTES);
+    // key size = 32
+    // key = "mmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmm";
+    
+
+    encrypted_buffer = encrypt_buffer(mem_file, file_size, key);
+    // decrypted_buffer = decrypt_buffer(encrypted_buffer, key);
+    
+
+
 }
 
 
--- logappend.c
+++ logappend.c
@@ -10,10 +10,8 @@
 #include <err.h>
 #include <errno.h>
 #include <assert.h>
-#include <sodium.h>
 
 #include "data.h"
-#include "fileEncryptionTest.h"
 
 char salt[] = "XXXXXXXXXXXXXXXXXXXX";
 char CTUID[] = "XXXXXXXXXXXXXXXXXXXX";
@@ -28,10 +26,8 @@ typedef struct _CmdLineResult {
   char*   name;
   int     name_len;
   int     roomID;
-  char*   logpath_pre;
-  char*   logpath_post;
-  int     logpath_pre_len;
-  int     logpath_post_len;
+  char*   logpath;
+  int     logpath_len;
 } CmdLineResult;
 
 
@@ -148,11 +144,10 @@ CmdLineResult parse_cmdline(int argc, char *argv[], int is_batch) {
   
   //pick up the positional argument for log path
   if(optind < argc) {
-    R.logpath_post_len = strlen(argv[optind]) + 1;
-    logpath = malloc(R.logpath_post_len);
-    memcpy(logpath, argv[optind], R.logpath_post_len);
-    R.logpath_post = logpath;
-    R.logpath_pre =  strcat(logpath, "_pre");
+    R.logpath_len = strlen(argv[optind]) + 1;
+    logpath = malloc(R.logpath_len);
+    memcpy(logpath, argv[optind], R.logpath_len);
+    R.logpath = logpath;
   }
   else if (batchfile==NULL) {
     // printf("Input missing log file.\n");
@@ -197,8 +192,8 @@ void print_cmdline(CmdLineResult R){
     printf("name: %s\n", R.name);
     printf("name_len: %d\n", R.name_len);
     printf("roomID: %d\n", R.roomID);
-    printf("logpath: %s\n", R.logpath_post);
-    printf("logpath_len: %d\n", R.logpath_post_len);
+    printf("logpath: %s\n", R.logpath);
+    printf("logpath_len: %d\n", R.logpath_len);
 }
 
 void print_logentry(LogEntry L){
@@ -224,20 +219,9 @@ int main(int argc, char *argv[]) {
     // First step: check if log file exists.
 
     // If log file doesn't exist, create a new file and write token
-    if( access( R.logpath_post, F_OK ) != 0 ){
+    if( access( R.logpath, F_OK ) != 0 ){
         // Create new log
-        // printf("Log file doesn't currently exists. Opening new log file.\n");
-      
-        // char *pre_encrypt_logpath;
-        // pre_encrypt_logpath = strcat(R.logpath, "_pre");
-        // printf("%s", pre_encrypt_logpath);
-
-
-////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
-
-
-
-        log_fp = fopen(R.logpath_pre, "w+");
+        log_fp = fopen(R.logpath, "w+");
         // Write token to beginning of file
         // token_len(int)+token(str)
         char token_len_str[4];
@@ -245,36 +229,12 @@ int main(int argc, char *argv[]) {
         fwrite(token_len_str, 1, 4, log_fp);
         fwrite(R.token, 1, R.token_len, log_fp);
         fclose(log_fp);
-
-
-        unsigned char key[crypto_secretstream_xchacha20poly1305_KEYBYTES];
-        crypto_secretstream_xchacha20poly1305_keygen(key);
-        
-        if (sodium_init() != 0) {
-            return 1;
-        }
-    
-        // char* encFile[] = strncat(R.logpath)
-        if (encrypt(R.logpath_pre, R.logpath_post, key) != 0) {
-            printf("Error\n");
-            return 1;
-        }
-        printf("%s\n", R.logpath_post);
-
-
-        // must encrypt right here
-
-
-
     }
 
     // Second step: check if token matches the one in existing log
 
-///////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
-
-
     // Open log read-only
-    log_fp = fopen(R.logpath_post, "r");
+    log_fp = fopen(R.logpath, "r");
     char *buf_r;
     buf_r = malloc(4);
     num_read = fread(buf_r, 1, 4, log_fp);
@@ -370,31 +330,8 @@ int main(int argc, char *argv[]) {
     }
     fclose(log_fp);
 
-    // Adding file encryption right here:
-    // unsigned char key[crypto_secretstream_xchacha20poly1305_KEYBYTES];
-    
-    // crypto_secretstream_xchacha20poly1305_keygen(key);
-    
-    // if (sodium_init() != 0) {
-    //     return 1;
-    // }
-    // char* encFile[] = strncat(R.logpath)
-    // if (encrypt(R.logpath, "/tmp/original", key) != 0) {
-    //     printf("Error\n");
-    //     return 1;
-    // }
-    // printf("%s\n", R.logpath);
-
-
-
-
     // Final step: add command line as a new log entry
-    
-
-
-
-////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
-    log_fp = fopen(R.logpath_post, "a");
+    log_fp = fopen(R.logpath, "a");
     char *buf;
     int buf_len;
     LogEntry L;
